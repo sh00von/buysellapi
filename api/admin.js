6da3b0b7d2e48a6db9c03470ff80c417943bf794
sh00von <u2110015@student.cuet.ac.bd>
2024-09-13 22:55:33 +0600

    update

--- api/admin.js
+++ api/admin.js
@@ -1,4 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('../lib/dbConnect');
+const adminRouter = require('../admin'); // Import your admin router
 
-const adminRouter = require('../admin');
-app.use('/admin', adminRouter);
-module.exports = app;
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Use the AdminJS routes
+app.use('/admin', adminRouter); // AdminJS dashboard
+
+module.exports = (req, res) => {
+  app(req, res);
+};
--- api/halls.js
+++ api/halls.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // Use the hall routes
-app.use('/api/halls', hallRoutes);
+app.use('/api/hall', hallRoutes);
 
 module.exports = (req, res) => {
   app(req, res);
